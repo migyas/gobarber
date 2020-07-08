dfad5fc72bcff6e3b82dd111a0f3e53d0f95db53
Yan Dias <yansantos0712@gmail.com>
2020-07-07 21:15:13 -0300

    Tudo ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
-  <>
-    <h1>Hello World</h1>
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
